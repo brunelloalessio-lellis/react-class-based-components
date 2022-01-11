f97c1f6c15fa1501c43f3081e09bdad97b7f8a32
abrunello <brunello@bludata.com>
2022-01-11 12:38:03 +0100

    test of componentWillUnmount

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -8,44 +8,50 @@ const DUMMY_USERS = [
   { id: "u3", name: "Julie" },
 ];
 
-class UserFinder extends Component{
+class UserFinder extends Component {
+  constructor() {
+    super();
+    this.state = {
+      filteredUsers: [],
+      searchTerm: "",
+    };
+  }
 
-    constructor(){
-        super()
-        this.state={
-            filteredUsers: DUMMY_USERS,
-            searchTerm:''
-        }
+  searchChangeHandler(event) {
+    this.setState({
+      searchTerm: event.target.value,
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: DUMMY_USERS.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
     }
+  }
 
-    searchChangeHandler(event){
-        this.setState({
-            searchTerm: event.target.value
-        })
-    }
+  componentDidMount() {
+    setTimeout(() => {
+      //possible http request
+      this.setState({
+        filteredUsers: DUMMY_USERS,
+      });
+    }, 200);
+  }
 
-    componentDidUpdate(prevProps, prevState){
-
-        if(prevState.searchTerm !== this.state.searchTerm){
-            this.setState({
-                filteredUsers: DUMMY_USERS.filter((user) => user.name.includes(this.state.searchTerm))
-            })
-        }
-        
-    }
-
-    render(){
-
-        return (
-            <>
-      <div className={classes.finder}>
-        <input type="search" onChange={this.searchChangeHandler.bind(this)} />
-      </div>
-      <Users users={this.state.filteredUsers} />
-    </>
-        )
-    }
-
+  render() {
+    return (
+      <>
+        <div className={classes.finder}>
+          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+        <Users users={this.state.filteredUsers} />
+      </>
+    );
+  }
 }
 
 export default UserFinder;
