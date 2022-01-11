a5c03ac1a232e5dd838d87a6da89419d24953aa9
abrunello <brunello@bludata.com>
2022-01-11 12:34:41 +0100

    converted UserFinder arrow function cmp to class based react component using componentDidUpdate

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { Component } from "react";
 import classes from "./UserFinder.module.css";
 import Users from "./Users";
 
@@ -8,28 +8,44 @@ const DUMMY_USERS = [
   { id: "u3", name: "Julie" },
 ];
 
-const UserFinder = () => {
-  const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
-  const [searchTerm, setSearchTerm] = useState("");
+class UserFinder extends Component{
 
-  useEffect(() => {
-    setFilteredUsers(
-      DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
-    );
-  }, [searchTerm]);
+    constructor(){
+        super()
+        this.state={
+            filteredUsers: DUMMY_USERS,
+            searchTerm:''
+        }
+    }
 
-  const searchChangeHandler = (event) => {
-    setSearchTerm(event.target.value);
-  };
+    searchChangeHandler(event){
+        this.setState({
+            searchTerm: event.target.value
+        })
+    }
 
-  return (
-    <>
+    componentDidUpdate(prevProps, prevState){
+
+        if(prevState.searchTerm !== this.state.searchTerm){
+            this.setState({
+                filteredUsers: DUMMY_USERS.filter((user) => user.name.includes(this.state.searchTerm))
+            })
+        }
+        
+    }
+
+    render(){
+
+        return (
+            <>
       <div className={classes.finder}>
-        <input type="search" onChange={searchChangeHandler} />
+        <input type="search" onChange={this.searchChangeHandler.bind(this)} />
       </div>
-      <Users users={filteredUsers} />
+      <Users users={this.state.filteredUsers} />
     </>
-  );
-};
+        )
+    }
+
+}
 
 export default UserFinder;
